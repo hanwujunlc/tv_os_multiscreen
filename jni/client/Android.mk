2754ec1e6963d306aa55da2a8b51a11c02878747
hanwujunlc <hanwujunlc@126.com>
2016-12-02 18:25:51 +0800

    new client code

--- jni/client/Android.mk
+++ jni/client/Android.mk
@@ -4,21 +4,28 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE:= TVOSClient
 
+
 LOCAL_C_INCLUDES := $(LOCAL_PATH)								\
 					$(LOCAL_PATH)/../sys_inc/system/core/include	\
 					$(LOCAL_PATH)/../sys_inc/frameworks/native/include	\
 					$(LOCAL_PATH)/../sys_inc/external						\
 					$(LOCAL_PATH)/../sys_inc/coocaa_os/LocalSocketIPC/inc	\
 					$(LOCAL_PATH)/../sys_inc/coocaa_os/CoocaaApiSDK/inc		\
+					$(LOCAL_PATH)/../sys_inc/coocaa_os/CoocaaApiSDK/inc		\
+					$(LOCAL_PATH)/../common \
+
+					
+LOCAL_SRC_FILES += \
+	../common/DataPacket.cpp \
+	../common/DEData.cpp \
+	../common/TCPSocket.cpp \
+	../common/UDPSocket.cpp \
+	../common/cJSON.c \
 
 
 LOCAL_SRC_FILES += \
-	DataPacket.cpp \
-	DEData.cpp \
-	clientTest.cpp \
-	TCPSocket.cpp \
-	UDPSocket.cpp \
-	cJSON.c
+	clientTest.cpp
+
 
 LOCAL_CFLAGS += -Wno-multichar -fno-rtti -fexceptions -fpermissive -std=c++11 -Wno-unused-parameter -Wno-reorder -DHAVE_SYS_UIO_H -DHAVE_SYS_LINUX_CAPABILITY_H
 LOCAL_LDFLAGS += -fPIE -pie 
